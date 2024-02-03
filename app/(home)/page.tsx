99a57bf755f1c6aee4745897fd0ac21622354f91
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-02-03 15:46:30 -0300

    feat: add initial booking-item component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,26 +1,31 @@
-import { format } from "date-fns"
-import { ptBR } from "date-fns/locale"
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-import { Header } from "../components/header"
-import { Search } from "./components/search"
+import { BookingItem } from "../components/booking-item";
+import { Header } from "../components/header";
+import { Search } from "./components/search";
 
 export default function Home() {
   const today = format(new Date(), "iiii',' d 'de' MMMM", {
     locale: ptBR,
-  })
+  });
 
   return (
     <>
       <Header />
 
-      <div className="px-5 mt-6">
+      <div className="mt-6 px-5">
         <h2 className="text-xl font-bold">Olá William!</h2>
-        <p className="capitalize text-sm">{today}</p>
+        <p className="text-sm capitalize">{today}</p>
       </div>
 
-      <div className="px-5 mt-6">
+      <div className="mt-6 px-5">
         <Search />
       </div>
+
+      <div className="mt-6 px-5">
+        <BookingItem title="AGENDAMENTOS" />
+      </div>
     </>
-  )
+  );
 }
