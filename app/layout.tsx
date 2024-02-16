9e8837ef9246049bdefbd997acb37b49acf56f20
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-02-16 15:43:34 -0300

    style: ensure footer is always on the bottom page

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>
+      <body className={`${inter.className} dark flex flex-col`}>
         <AuthProvider>
-          {children}
+          <div className="flex-1">{children}</div>
           <Toaster />
           <Footer />
         </AuthProvider>
