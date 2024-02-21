341e3b8b6bf6ef8f1737a65490ff9aac8f4ea72d
chocolesdeveloper <williamferreira.programador@gmail.com>
2024-02-21 14:12:01 -0300

    feat: add container class

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -51,103 +51,105 @@ export default async function Home() {
     <>
       <Header />
 
-      <div className="relative z-10 flex items-center gap-32 overflow-hidden px-5 lg:h-[482px] lg:px-32">
-        <div className="flex w-full flex-col lg:min-w-[440px] lg:gap-11">
-          <div className="mt-6 lg:mt-0">
-            <h2 className="text-xl font-bold">
-              {session?.user?.name
-                ? `Olá, ${session?.user.name.split(" ")[0]}!`
-                : "Olá, vamos agendar um corte?"}
+      <div
+        className="absolute inset-0 top-24 -z-10 hidden h-[482px] lg:block"
+        style={{
+          background: "url(bg-home.png) no-repeat center/cover",
+        }}
+      />
+
+      <div className="container">
+        <div className="flex items-center gap-32 overflow-hidden px-5 lg:h-[482px] lg:px-32">
+          <div className="flex w-full flex-col lg:min-w-[440px] lg:gap-11">
+            <div className="mt-6 lg:mt-0">
+              <h2 className="text-xl font-bold">
+                {session?.user?.name
+                  ? `Olá, ${session?.user.name.split(" ")[0]}!`
+                  : "Olá, vamos agendar um corte?"}
+              </h2>
+              <p className="text-sm capitalize">{today}</p>
+            </div>
+
+            <div className="mt-6 ">
+              <Search />
+            </div>
+
+            <div className="mt-6">
+              {confirmedBookings.length > 0 && (
+                <>
+                  <h2 className="mb-3 text-xs font-bold uppercase leading-4 text-gray-400">
+                    agendamentos
+                  </h2>
+                  <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+                    <Carousel className="w-full">
+                      <CarouselContent className="flex  gap-6 px-5">
+                        {confirmedBookings.map((booking) => (
+                          <BookingItem key={booking.id} booking={booking} />
+                        ))}
+                      </CarouselContent>
+                    </Carousel>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className="relative mt-20 hidden min-w-[620px] lg:block">
+            <h2 className="mb-3 px-5 text-xs font-bold uppercase leading-4 text-gray-400">
+              RECOMENDADOS
             </h2>
-            <p className="text-sm capitalize">{today}</p>
-          </div>
-
-          <div className="mt-6 ">
-            <Search />
-          </div>
-
-          <div className="mt-6">
-            {confirmedBookings.length > 0 && (
-              <>
-                <h2 className="mb-3 text-xs font-bold uppercase leading-4 text-gray-400">
-                  agendamentos
-                </h2>
-                <div className="flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-                  <Carousel className="w-full">
-                    <CarouselContent className="flex  gap-6 px-5">
-                      {confirmedBookings.map((booking) => (
-                        <BookingItem key={booking.id} booking={booking} />
-                      ))}
-                    </CarouselContent>
-                  </Carousel>
-                </div>
-              </>
-            )}
+            <Carousel>
+              <CarouselContent className="flex gap-6">
+                {recommededBarberShops.map((barbershop) => (
+                  <CarouselItem key={barbershop.id} className="max-w-[200px]">
+                    <BarbershopItem barbershop={barbershop} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselNext />
+              <CarouselPrevious />
+            </Carousel>
           </div>
         </div>
 
-        <div className="mt-20 hidden min-w-[620px] lg:block">
+        <div className="mt-6">
           <h2 className="mb-3 px-5 text-xs font-bold uppercase leading-4 text-gray-400">
-            RECOMENDADOS
+            Barbearias
           </h2>
-          <Carousel>
-            <CarouselContent className="flex gap-6">
-              {recommededBarberShops.map((barbershop) => (
-                <CarouselItem key={barbershop.id} className="max-w-[200px]">
-                  <BarbershopItem barbershop={barbershop} />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselNext />
-            <CarouselPrevious />
-          </Carousel>
+          <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+            <Carousel>
+              <CarouselContent className="flex gap-2 lg:gap-6">
+                {barbershops.map((barbershop) => (
+                  <CarouselItem
+                    key={barbershop.id}
+                    className="max-w-[200px] px-2"
+                  >
+                    <BarbershopItem barbershop={barbershop} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </div>
         </div>
 
-        <div
-          className="absolute inset-0 -z-10 hidden lg:block"
-          style={{
-            background: "url(bg-home.png) no-repeat center/cover",
-          }}
-        />
-      </div>
-
-      <div className="mt-6">
-        <h2 className="mb-3 px-5 text-xs font-bold uppercase leading-4 text-gray-400">
-          Barbearias
-        </h2>
-        <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
-          <Carousel>
-            <CarouselContent className="flex gap-2 lg:gap-6">
-              {barbershops.map((barbershop) => (
-                <CarouselItem
-                  key={barbershop.id}
-                  className="max-w-[200px] px-2"
-                >
-                  <BarbershopItem barbershop={barbershop} />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
-        </div>
-      </div>
-
-      <div className="mb-[4.5rem] mt-6">
-        <h2 className="mb-3 px-5 text-xs font-bold uppercase leading-4 text-gray-400">
-          Populares
-        </h2>
-        <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
-          <Carousel>
-            <CarouselContent className="flex gap-2 lg:gap-6">
-              {recommededBarberShops.map((barbershop) => (
-                <CarouselItem
-                  key={barbershop.id}
-                  className="max-w-[200px] px-2"
-                >
-                  <BarbershopItem barbershop={barbershop} />
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </Carousel>
+        <div className="mb-[4.5rem] mt-6">
+          <h2 className="mb-3 px-5 text-xs font-bold uppercase leading-4 text-gray-400">
+            Populares
+          </h2>
+          <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+            <Carousel>
+              <CarouselContent className="flex gap-2 lg:gap-6">
+                {recommededBarberShops.map((barbershop) => (
+                  <CarouselItem
+                    key={barbershop.id}
+                    className="max-w-[200px] px-2"
+                  >
+                    <BarbershopItem barbershop={barbershop} />
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+            </Carousel>
+          </div>
         </div>
       </div>
     </>
